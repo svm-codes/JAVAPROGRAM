3ac9297f65ca8b68971dd970de6747875a9754b4
svm-codes <shiva13380@gmail.com>
2025-08-23 00:45:22 +0530

    first commit

---
+++ Valuesoftwovariables.java
@@ -0,0 +1,11 @@
+public class Valuesoftwovariables {
+    public static void main (String args[]){
+
+        int a,b;
+        a=2;
+        b=3;
+        System.out.println("a="+a+" b="+b);
+    }
+
+    
+}
