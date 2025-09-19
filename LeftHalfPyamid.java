a5c7c3e596dfe359f935e88b082bd67cbf352ec7
svm-codes <shiva13380@gmail.com>
2025-09-19 18:00:46 +0530

    LeftHalfPyramid

---
+++ LeftHalfPyamid.java
@@ -0,0 +1,12 @@
+public class LeftHalfPyamid {
+    
+    public static void main(String[]args){
+        System.out.println("     *");
+         System.out.println("   * *");
+         System.out.println("  * * *");
+         System.out.println(" * * * *");
+         System.out.println("* * * * *");
+         
+
+    }
+}
