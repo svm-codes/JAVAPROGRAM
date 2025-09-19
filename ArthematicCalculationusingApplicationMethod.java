c0a17ab26bbed1af76049e3d8b57cdcfa9cf7898
svm-codes <shiva13380@gmail.com>
2025-09-19 17:21:54 +0530

    StarPattern

---
+++ ArthematicCalculationusingApplicationMethod.java
@@ -0,0 +1,14 @@
+public class ArthematicCalculationusingApplicationMethod {
+    public static void main(String[]args){
+        int a,b,c;
+        a=10;
+        b=20;
+        c=30;
+        a+=3;
+        b+=a;
+        c-=a;
+        System.out.println("a = "+a+"b ="+b+"c ="+c);
+
+    }
+    
+}
