91357ee8e513e1d1f66471dfa66a2de2b8d96fcc
svm-codes <shiva13380@gmail.com>
2025-09-20 17:24:17 +0530

    Pattern print in single statement

---
+++ Patternprintsinglestatement.java
@@ -0,0 +1,8 @@
+public class Patternprintsinglestatement {
+    public static void main(String[]args){
+        System.out.println(" * \n * * \n * * *\n * * * *\n * * * * * \n");
+        System.out.println(" * * * * * \n * * * * \n * * * \n * * \n * \n");
+        System.out.print("     * \n    * * \n   * * * \n  * * * * \n * * * * *");
+    }
+    
+}
