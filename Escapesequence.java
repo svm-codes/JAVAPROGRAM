8e91bfffe205951565a2d1cb8aa7ac6e889cee03
svm-codes <shiva13380@gmail.com>
2025-09-20 16:56:59 +0530

    Escapesequence

--- Escapesequence.java
+++ Escapesequence.java
@@ -1,6 +1,10 @@
 public class Escapesequence {
     public static void main(String[]args){
         System.out.println("Hii \"SHIVAM\"...");
+        System.out.println("Hii \nSHIVAM");
+        System.out.println("Hii \\SHIVAM\\");
+        System.out.println("Hii\bSHIVAM");
+        System.out.println("Hii \tSHIVAM");
     }
     
 }
